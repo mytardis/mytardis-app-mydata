fe633b6fb1906fe59de459f06091d9331f03d8fb
James Wettenhall <James.Wettenhall@monash.edu>
2015-04-23 13:32:38 +1000

    Fixed model path for Uploader

--- admin.py
+++ admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 import models
 
-admin.site.register(Uploader)
+admin.site.register(models.Uploader)
 admin.site.register(models.UploaderStagingHost)
 admin.site.register(models.UploaderRegistrationRequest)
