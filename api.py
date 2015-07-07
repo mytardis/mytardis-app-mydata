66cd82ba424898b7fb05928526d91804fa18cf2d
James Wettenhall <James.Wettenhall@monash.edu>
2015-07-07 23:23:27 +1000

    Added missing import.

--- api.py
+++ api.py
@@ -7,6 +7,7 @@ import traceback
 from datetime import datetime
 
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from tastypie import fields
 from tastypie.constants import ALL_WITH_RELATIONS
