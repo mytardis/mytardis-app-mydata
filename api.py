5ad78344b83ec2573e2ba53f9f6613082e9774e8
James Wettenhall <James.Wettenhall@monash.edu>
2018-02-02 14:00:45 +1100

    Extending the MyTardis API's DataFileResource class meant that DataFile lookups were too slow for MyData, because we were getting DataFile metadata which wasn't needed.  So now we're defining a new API resource for DataFile lookups, rather than using inheritance to extend the MyTardis API's DataFileResource.

--- api.py
+++ api.py
@@ -7,6 +7,7 @@ import traceback
 from datetime import datetime
 
 from django.conf import settings
+from django.conf.urls import url
 from django.contrib.auth.models import User
 from django.contrib.sites.models import Site
 from django.core import mail
@@ -17,6 +18,7 @@ from django.template import Context
 from tastypie import fields
 from tastypie.constants import ALL_WITH_RELATIONS
 from tastypie.exceptions import ImmediateHttpResponse
+from tastypie.utils import trailing_slash
 from ipware.ip import get_ip
 
 import tardis.tardis_portal.api
@@ -27,7 +29,9 @@ from tardis.tardis_portal.models.parameters import Schema
 from tardis.tardis_portal.models.parameters import ParameterName
 from tardis.tardis_portal.models.parameters import ExperimentParameter
 from tardis.tardis_portal.models.parameters import ExperimentParameterSet
+from tardis.tardis_portal.models.datafile import DataFile
 from tardis.tardis_portal.models.datafile import DataFileObject
+from tardis.tardis_portal.models.datafile import compute_checksums
 
 from models.uploader import Uploader
 from models.uploader import UploaderRegistrationRequest
@@ -458,16 +462,61 @@ class ExperimentAppResource(tardis.tardis_portal.api.ExperimentResource):
                                                                **kwargs)
 
 
-class DataFileAppResource(tardis.tardis_portal.api.DataFileResource):
-    '''Extends MyTardis's API for DataFiles to make use of the
+class DataFileAppResource(tardis.tardis_portal.api.MyTardisModelResource):
+    '''
+    Replaces MyTardis's API for DataFiles to make use of the
     Uploader model's approved_storage_box in staging uploads
     (e.g. from MyData)
+
+    This class used to extend (inherit from) the MyTardis API's
+    DataFileResource class, but the metadata included in the bundle is
+    not needed for MyData's DataFile lookups, so now we're replacing
+    that class, rather than extending it.
     '''
+    dataset = fields.ForeignKey(tardis.tardis_portal.api.DatasetResource, 'dataset')
+    datafile = fields.FileField()
+    replicas = fields.ToManyField(
+        'tardis.tardis_portal.api.ReplicaResource',
+        'file_objects',
+        related_name='datafile', full=True, null=True)
     temp_url = None
 
-    class Meta(tardis.tardis_portal.api.DataFileResource.Meta):
+    class Meta(tardis.tardis_portal.api.MyTardisModelResource.Meta):
+        queryset = DataFile.objects.all()
+        filtering = {
+            'directory': ('exact', 'startswith'),
+            'dataset': ALL_WITH_RELATIONS,
+            'filename': ('exact', ),
+        }
+        ordering = [
+            'filename',
+            'modification_time'
+        ]
         # This will be mapped to mydata_dataset_file by MyTardis's urls.py:
         resource_name = 'dataset_file'
+
+    def hydrate(self, bundle):
+        if 'attached_file' in bundle.data:
+            # have POSTed file
+            newfile = bundle.data['attached_file'][0]
+            compute_md5 = getattr(settings, 'COMPUTE_MD5', True)
+            compute_sha512 = getattr(settings, 'COMPUTE_SHA512', True)
+            if (compute_md5 and 'md5sum' not in bundle.data) or \
+                    (compute_sha512 and 'sha512sum' not in bundle.data):
+                checksums = compute_checksums(newfile, close_file=False)
+                if compute_md5:
+                    bundle.data['md5sum'] = checksums['md5sum']
+                if compute_sha512:
+                    bundle.data['sha512sum'] = checksums['sha512sum']
+
+            if 'replicas' in bundle.data:
+                for replica in bundle.data['replicas']:
+                    replica.update({'file_object': newfile})
+            else:
+                bundle.data['replicas'] = [{'file_object': newfile}]
+
+            del(bundle.data['attached_file'])
+        return bundle
 
     def obj_create(self, bundle, **kwargs):
         '''
@@ -476,8 +525,7 @@ class DataFileAppResource(tardis.tardis_portal.api.DataFileResource):
         If a duplicate key error occurs, responds with HTTP Error 409: CONFLICT
         '''
         try:
-            retval = super(tardis.tardis_portal.api.DataFileResource, self)\
-                .obj_create(bundle, **kwargs)
+            retval = super(DataFileAppResource, self).obj_create(bundle, **kwargs)
         except IntegrityError as err:
             if "duplicate key" in str(err):
                 raise ImmediateHttpResponse(HttpResponse(status=409))
@@ -520,6 +568,52 @@ class DataFileAppResource(tardis.tardis_portal.api.DataFileResource):
             dfo.save()
             self.temp_url = dfo.get_full_path()
         return retval
+
+    def post_list(self, request, **kwargs):
+        response = super(DataFileAppResource, self).post_list(request, **kwargs)
+        if self.temp_url is not None:
+            response.content = self.temp_url
+            self.temp_url = None
+        return response
+
+    def prepend_urls(self):
+        return [
+            url(r"^(?P<resource_name>%s)/(?P<pk>\w[\w/-]*)/download%s$" %
+                (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('download_file'), name="api_download_file"),
+            url(r"^(?P<resource_name>%s)/(?P<pk>\w[\w/-]*)/verify%s$" %
+                (self._meta.resource_name, trailing_slash()),
+                self.wrap_view('verify_file'), name="api_verify_file"),
+        ]
+
+    def deserialize(self, request, data, format=None):
+        '''
+        from https://github.com/toastdriven/django-tastypie/issues/42
+        modified to deserialize json sent via POST. Would fail if data is sent
+        in a different format.
+        uses a hack to get back pure json from request.POST
+        '''
+        if not format:
+            format = request.META.get('CONTENT_TYPE', 'application/json')
+        if format == 'application/x-www-form-urlencoded':
+            return request.POST
+        if format.startswith('multipart'):
+            jsondata = request.POST['json_data']
+            data = super(DataFileAppResource, self).deserialize(
+                request, jsondata, format='application/json')
+            data.update(request.FILES)
+            return data
+        return super(DataFileAppResource, self).deserialize(request, data, format)
+
+    def put_detail(self, request, **kwargs):
+        '''
+        from https://github.com/toastdriven/django-tastypie/issues/42
+        '''
+        if request.META.get('CONTENT_TYPE').startswith('multipart') and \
+                not hasattr(request, '_body'):
+            request._body = ''
+
+        return super(DataFileAppResource, self).put_detail(request, **kwargs)
 
 
 class ReplicaAppResource(tardis.tardis_portal.api.ReplicaResource):
