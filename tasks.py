bee439ac64e425891bbb61893372b97976301a1f
Dmitry Yakhnov <dmitry@yakhnov.info>
2021-06-10 11:47:19 +1000

    Updates

--- tasks.py
+++ tasks.py
@@ -1,4 +1,5 @@
 import os
+import math
 import logging
 
 from django.conf import settings
@@ -18,7 +19,7 @@ def complete_chunked_upload(dfo_id):
     """
     Assembly data file from chunks
     """
-    def make_file(dfo, chunk_ids, data_path):
+    def make_file(dfo, chunk_ids, data_path, chunk_size):
         """
         Reassembly file from chunks
         """
@@ -30,18 +31,25 @@ def complete_chunked_upload(dfo_id):
         dst_dir = os.path.dirname(dst_path)
         if not os.path.exists(dst_dir):
             os.makedirs(dst_dir)
-        with open(dst_path, "wb") as dst:
-            for chunk_id in chunk_ids:
-                file_path = os.path.join(data_path, chunk_id)
-                logger.info("Complete file %s chunk %s" % (dfo.id, chunk_id))
-                with open(file_path, "rb") as src:
-                    while True:
-                        data = src.read(settings.CHUNK_COPY_SIZE)
-                        dst.write(data)
-                        if len(data) != settings.CHUNK_COPY_SIZE:
-                            src.close()
-                            break
-            dst.close()
+        if os.path.exists(dst_path):
+            file_size = os.stat(dst_path).st_size
+            file_offset = max(math.ceil(file_size/chunk_size)-1, 0) * chunk_size
+            dst = open(dst_path, "r+b")
+            dst.seek(file_offset)
+        else:
+            dst = open(dst_path, "wb")
+
+        for chunk_id in chunk_ids:
+            file_path = os.path.join(data_path, chunk_id)
+            logger.info("Complete file %s chunk %s" % (dfo.id, chunk_id))
+            with open(file_path, "rb") as src:
+                while True:
+                    data = src.read(settings.CHUNK_COPY_SIZE)
+                    dst.write(data)
+                    if len(data) != settings.CHUNK_COPY_SIZE:
+                        src.close()
+                        break
+        dst.close()
 
     logger.info("Complete file %s" % dfo_id)
     dfo = DataFileObject.objects.get(id=dfo_id)
@@ -53,7 +61,7 @@ def complete_chunked_upload(dfo_id):
         # Copy chunks to a final destination
         try:
             logger.debug("Complete file %s assembly" % dfo_id)
-            make_file(dfo, chunk_ids, data_path)
+            make_file(dfo, chunk_ids, data_path, chunks[0].size)
             logger.debug("Complete file %s file ready" % dfo.id)
         except Exception as e:
             logger.error("Complete file %s error %s" % (dfo_id, str(e)))
@@ -81,33 +89,35 @@ def complete_chunked_upload(dfo_id):
     return True
 
 
+@tardis_app.task(name="tardis_portal.remove_chunked_upload", ignore_result=True)
+def remove_chunked_upload(dfo_id):
+    logger.debug("Remove incomplete upload %s" % dfo_id)
+    chunks = Chunk.objects.filter(dfo_id=dfo_id).order_by("offset")
+    chunk_ids = [chunk.chunk_id for chunk in chunks]
+    if len(chunk_ids) != 0:
+        data_path = os.path.join(settings.CHUNK_STORAGE, str(dfo_id))
+        for chunk_id in chunk_ids:
+            try:
+                os.remove(os.path.join(data_path, chunk_id))
+            except Exception as e:
+                logger.error(str(e))
+        try:
+            os.rmdir(data_path)
+        except Exception as e:
+            logger.error(str(e))
+        chunks.delete()
+
+
 @tardis_app.task(name="tardis_portal.chunks_cleanup", ignore_result=True)
 def chunks_cleanup():
     """
     Find lost chunks (due to incomplete uploads) and cleanup
     """
-    chunks = Chunk.objects.order_by("dfo_id").values("dfo_id").distinct()
-    cleanup = []
-    for chunk in chunks:
-        dfo = DataFileObject.objects.filter(id=chunk["dfo_id"])
+    uploads = Chunk.objects.order_by("dfo_id").values("dfo_id").distinct()
+    for upload in uploads:
+        dfo = DataFileObject.objects.filter(id=upload["dfo_id"])
         if len(dfo) == 0:
-            cleanup.append(chunk["dfo_id"])
-    for dfo_id in cleanup:
-        logger.debug("Cleanup for incomplete upload %s" % dfo_id)
-        chunks = Chunk.objects.filter(dfo_id=dfo_id).order_by("offset")
-        chunk_ids = [chunk.chunk_id for chunk in chunks]
-        if len(chunk_ids) != 0:
-            data_path = os.path.join(settings.CHUNK_STORAGE, str(dfo_id))
-            for chunk_id in chunk_ids:
-                try:
-                    os.remove(os.path.join(data_path, chunk_id))
-                except Exception as e:
-                    logger.error(str(e))
-            try:
-                os.rmdir(data_path)
-            except Exception as e:
-                logger.error(str(e))
-            chunks.delete()
+            remove_chunked_upload.apply_async(upload["dfo_id"])
 
 
 @tardis_app.task(name="tardis_portal.chunks_complete", ignore_result=True)
@@ -115,12 +125,13 @@ def chunks_complete():
     """
     Find and try to complete uploads not processed straight after upload
     """
-    chunks = Chunk.objects.order_by("dfo_id").values("dfo_id").distinct()
-    for chunk in chunks:
-        dfo = DataFileObject.objects.get(id=chunk["dfo_id"])
-        last_chunk = Chunk.objects.filter(
-            dfo_id=dfo.id
-        ).order_by("-offset")[0]
-        offset = min(last_chunk.offset + last_chunk.size, dfo.datafile.size)
-        if offset == dfo.datafile.size:
-            complete_chunked_upload.apply_async(args=[dfo.id])
+    uploads = Chunk.objects.order_by("dfo_id").values("dfo_id").distinct()
+    for upload in uploads:
+        dfo = DataFileObject.objects.filter(id=upload["dfo_id"])
+        if len(dfo) != 0:
+            last_chunk = Chunk.objects.filter(
+                dfo_id=dfo.id
+            ).order_by("-offset")[0]
+            offset = min(last_chunk.offset + last_chunk.size, dfo.datafile.size)
+            if offset == dfo.datafile.size:
+                complete_chunked_upload.apply_async(args=[dfo.id])
